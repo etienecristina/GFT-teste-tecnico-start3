1e785ce7c96a254dd6fc38b8273aaf0835f767da
Etiene Cristina <etiene.scristina@gmail.com>
2022-02-08 18:07:20 -0300

    versão final

--- src/exercicio4/Exercicio4.java
+++ src/exercicio4/Exercicio4.java
@@ -1,13 +1,27 @@
 package exercicio4;
 
+/**
+ * Dado um número inteiro maior que 0, faça uma operação que retorne o valor invertido dos
+ * números, a aplicação de console deverá imprimir os valores iniciais e os alterados na
+ * mesma linha.
+ * Ex: 123 → 321
+ * Não é necessário solicitar a entrada de dados, usar como exemplo as entradas abaixo como
+ * números inteiros:
+ * • 15485448
+ * • 2195498
+ * • 12
+ * • 98716549
+ * • 9999998999
+ * • 1654891
+ * **/
+
 public class Exercicio4 {
     public static void main(String[] args) {
         int inteiro1 = 15485448;
         int inteiro2 = 2195498;
         int inteiro3 = 12;
         int inteiro4 = 98716549;
-        //nao cabe no inteiro
-        //int inteiro5 = 9999998999;
+        //long inteiro5 = 9999998999;
         int inteiro6 = 1654891;
 
         System.out.println(inteiro1 + " → " + inverteNumeroInteiro(inteiro1));
